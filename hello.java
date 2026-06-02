ebc8927e2814166988380e0f90890305a429bb42
Vishok <p.vishok969@gmail.com>
2026-06-02 10:45:22 +0000

    java

---
+++ hello.java
@@ -0,0 +1,27 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int y=sc.nextInt();
+       int n=out(y);
+       System.out.println(n);
+       
+    }
+    
+    public static int out( int y){
+         Set<Integer> l=new HashSet<>();
+       l.add(0);
+       for(int i=0;i<=y;i++){
+           Set<Integer> t=new HashSet<>();
+           for(int k:l){
+               t.add(k+3);
+               t.add(k*2);
+           }
+           l=t;
+       }
+       return l.size();
+       
+    }
+}
---
+++ second.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+public class second {
+   
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       String str=sc.nextLine();
+       int t=0;
+       char a=String.tocharArray(str);
+       for(int i=0;i<str.length()-1;i++){
+           if(Character.isUpperCase(a[i])&& Character.isLowerCase(a[i+1]) || Character.isLowerCase(a[i])&& Character .isUpperCase(a[i+1])){
+               t++;
+           }
+       }
+       
+       System.out.println(t);
+   
+    }
+}
+}
