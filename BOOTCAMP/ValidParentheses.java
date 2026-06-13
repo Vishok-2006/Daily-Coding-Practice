006426a633f72e7b4b5ea4565223e81f92606a79
Vishok-2006 <pvishok969@gmail.com>
2026-06-13 12:59:48 +0530

    4

---
+++ BOOTCAMP/ValidParentheses.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+public class ValidParentheses{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+       
+        Stack<Character> res=new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c=='('|| c=='['  || c=='{'){
+                res.push(c);
+            }
+            else{
+                if(res.isEmpty()){
+                    System.out.println("false");
+                   return;
+                }
+                else{
+                    char t=res.pop();
+                    if((c==')'&& t!='(')||( c==']' && t!='[') ||( c=='}' && t!='{')){
+                        System.out.println("false");
+                        return;
+                }
+            }
+        }
+        
+    }
+    System.out.println(res.isEmpty());
+    sc.close();
+}
+}
