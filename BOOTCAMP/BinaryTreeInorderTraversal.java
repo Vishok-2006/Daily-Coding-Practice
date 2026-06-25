15adbfa50f0b0bc0b622ed0c0cfd202ca12daea2
Vishok <p.vishok969@gmail.com>
2026-06-25 19:04:30 +0530

    Add inorder traversal method for binary tree
    Implement inorder traversal for binary tree.

---
+++ BOOTCAMP/BinaryTreeInorderTraversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+       List<Integer> l=new LinkedList<>();
+        trav(root,l);
+        return l;
+    }
+    public static void trav(TreeNode root,List<Integer> l ){
+        if(root==null){
+            return ;
+
+        }
+        trav(root.left,l);
+        l.add(root.val);
+        trav(root.right,l);
+    }
+}
