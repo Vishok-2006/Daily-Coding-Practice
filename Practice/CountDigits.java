03decc8e99adb89dbbf1ddb8b363818362188f6f
Vishok <p.vishok969@gmail.com>
2026-08-02 05:13:51 +0000

    55

--- Practice/CountDigits.java
+++ Practice/CountDigits.java
@@ -1,5 +1,16 @@
 package Practice;
-
+import java.util.*;
 public class CountDigits {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print("The Count Is :"+Count(n));
+        sc.close();
+  }
+    public static int Count(int n){
+           if(n==0){
+           return 0;
+           }
+           return 1+Count(n/10);
+    }
 }
